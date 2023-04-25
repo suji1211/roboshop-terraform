dcc6aa3eedd001ee88a573a818f57349aa6eb666
suji <borra.sujani@gmail.com>
2023-04-25 10:37:40 -0400

    roboshop terraform instances

--- servers.tf
+++ servers.tf
@@ -71,6 +71,15 @@ resource "aws_instance" "shipping" {
   }
 }
 
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
 resource "aws_instance" "payment" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
