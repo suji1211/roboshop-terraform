9d8a97cadf7cba989c07126ca4c2697c9edac896
suji <borra.sujani@gmail.com>
2023-04-25 13:44:06 -0400

    roboshop terraform output

--- servers.tf
+++ servers.tf
@@ -11,9 +11,9 @@ output "ami" {
   resource "aws_instance" "frontend" {
     ami           = "ami-0b5a2b5b8f2be4ec2"
     instance_type = "t3.micro"
-  }
-  tags = {
-    Name = "frontend"
+    tags          = {
+      Name = "frontend"
+    }
   }
 output "frontend" {
   value = aws_instance.frontend.public_ip
