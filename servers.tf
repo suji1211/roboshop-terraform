0eeabbc1b796c2deb24b4a4c18e6f13ad8d7fd62
suji <borra.sujani@gmail.com>
2023-05-02 17:54:21 -0400

    loops

--- servers.tf
+++ servers.tf
@@ -7,32 +7,95 @@ data "aws_ami" "centos" {
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
-variable "instance_type" {
-  default = "t3.small"
+variable "components" {
+  default = {
+    frontend={
+      name = "frontend"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+      name = "mongodb"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.micro"
+    }
+    redis = {
+      name = "redis"
+      instance_type = "t3.small"
+    }
+    user = {
+      name = "user"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name = "cart"
+      instance_type = "t3.small"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name = "shipping"
+      instance_type = "t3.medium"
+    }
+    rabbitmq = {
+      name = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    payment = {
+      name = "payment"
+      instance_type = "t3.small"
+    }
+  }
 }
- variable "components"{
-   default = ["frontend", "mongodb", "catalogue","redis","mysql","cart","user","shipping","dispatch","rabbitmq","payment"]
- }
 
-variable "records" {
-  default = ["frontend-dev.sujianilsrisriyaan.online", "mongodb-dev.sujianilsrisriyaan.online","catalogue-dev.sujianilsrisriyaan.online","redis-dev.sujianilsrisriyaan.online","mysql-dev.sujianilsrisriyaan.online","cart-dev.sujianilsrisriyaan.online","user-dev.sujianilsrisriyaan.online","shipping-dev.sujianilsrisriyaan.online","dispatch-dev.sujianilsrisriyaan.online","rabbitmq-dev.sujianilsrisriyaan.online","payment-dev.sujianilsrisriyaan.online"]
-}
-  resource "aws_instance" "instance" {
-    count = length(var.components)
+resource "aws_instance" "instance" {
+    for_each = var.components
     ami           = data.aws_ami.centos.image_id
-    instance_type = var.instance_type
+    instance_type = each.value["instance_type"]
     vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
     tags          = {
-      Name = var.components[count.index]
+      Name = each.value["name"]
     }
   }
 
-resource "aws_route53_record" "dns_records" {
-  count =length(var.records)
-  zone_id = "Z00615461MLF0SCGQYI0V"
-  name    = var.records[count.index]
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance[count.index].private_ip]
-}
+
+
+
+
+
+
+
+#variable "instance_type" {
+#  default = "t3.small"
+# }
+# variable "components"{
+#   default = ["frontend", "mongodb", "catalogue","redis","mysql","cart","user","shipping","dispatch","rabbitmq","payment"]
+# }
+
+#variable "records" {
+#  default = ["frontend-dev.sujianilsrisriyaan.online", "mongodb-dev.sujianilsrisriyaan.online","catalogue-dev.sujianilsrisriyaan.online","redis-dev.sujianilsrisriyaan.online","mysql-dev.sujianilsrisriyaan.online","cart-dev.sujianilsrisriyaan.online","user-dev.sujianilsrisriyaan.online","shipping-dev.sujianilsrisriyaan.online","dispatch-dev.sujianilsrisriyaan.online","rabbitmq-dev.sujianilsrisriyaan.online","payment-dev.sujianilsrisriyaan.online"]
+#}
+#  resource "aws_instance" "instance" {
+#    count = length(var.components)
+#    ami           = data.aws_ami.centos.image_id
+#    instance_type = var.instance_type
+#    vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+#
+#    tags          = {
+#      Name = var.components[count.index]
+#    }
+#  }
+#
+#resource "aws_route53_record" "dns_records" {
+#  count =length(var.records)
+#  zone_id = "Z00615461MLF0SCGQYI0V"
+#  name    = var.records[count.index]
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.instance[count.index].private_ip]
+#}
