6a5d8eff1fd86d39a69eb5277781983c6685501a
suji <borra.sujani@gmail.com>
2023-05-03 17:08:44 -0400

    Dev&Prod

---
+++ env-prod/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-suji"
+key    = "roboshop/prod/terraform.tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,8 +1,3 @@
 terraform {
-
-  backend "s3" {
-    bucket = "terraform-suji"
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
