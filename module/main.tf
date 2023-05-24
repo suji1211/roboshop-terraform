c215cab6f0954e59f9ba5e1ab8ecfb9c9bebc3be
suji <borra.sujani@gmail.com>
2023-05-24 17:29:51 -0400

    policy

--- module/main.tf
+++ module/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "instance" {
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
   tags = {
-    Name = var.component_name
+    Name = local.name
   }
 }
 
