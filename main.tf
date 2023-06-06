50fe2af59cdda6335c15927d1dff2c8674f9cbf7
suji <borra.sujani@gmail.com>
2023-06-06 17:53:03 -0400

    prometheus

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/suji1211/tf-module-vpc.git"
 
-   for_each = var.vpc
+  for_each = var.vpc
   cidr_block = each.value["cidr_block"]
   subnets = each.value["subnets"]
   tags = local.tags
