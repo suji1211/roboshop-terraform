829b96832416dc100d44019768aced6935c8c8f0
suji <borra.sujani@gmail.com>
2023-06-08 16:35:47 -0400

    prometheus

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,4 +1,5 @@
 env = "dev"
+bastion_cidr     = ["172.31.5.241/32"]
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
--- vars.tf
+++ vars.tf
@@ -1,2 +1,16 @@
 variable "env" {}
 variable "vpc" {}
+variable "app" {}
+variable "monitor_cidr" {}
+variable "bastion_cidr" {}
+variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_vpc_rtid" {}
+variable "domain_name" {}
+variable "domain_id" {}
+variable "kms_arn" {}
+variable "docdb" {}
+variable "rds" {}
+variable "elasticache" {}
+variable "rabbitmq" {}
+variable "alb" {}
