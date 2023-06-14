5896b7ab4e593ffc030af25f7cc31b618a1295fc
suji <borra.sujani@gmail.com>
2023-06-14 11:02:04 -0400

    module alb

--- vars.tf
+++ vars.tf
@@ -6,8 +6,8 @@ variable "bastion_cidr" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_vpc_rtid" {}
-variable "domain_name" {}
-variable "domain_id" {}
+#variable "domain_name" {}
+#variable "domain_id" {}
 variable "kms_arn" {}
 variable "docdb" {}
 variable "rds" {}
