c4cad9e3b517f2754727934cf8f0e0535d28c543
suji <borra.sujani@gmail.com>
2023-06-14 11:04:51 -0400

    module alb

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ module "app" {
   domain_name  = var.domain_name
   domain_id    = var.domain_id
   kms_arn      = var.kms_arn
-#  monitor_cidr = var.monitor_cidr
+  monitor_cidr = var.monitor_cidr
 }
 #
 #### Load Runner
